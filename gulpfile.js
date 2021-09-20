9caab89f1c684d0fc9042e0adddd78f1128be4be
Seshal Jain <seshaljain11@gmail.com>
2021-09-20 11:51:34 +0530

    Add files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const bs = require('browser-sync').create();
+const terser = require('gulp-terser');
+
+// Build CSS
+function styles() {
+    return src('app/css/style.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(dest('dist', { sourcemaps: '.' }))
+        .pipe(bs.stream());
+}
+
+// Minify JS
+function scripts() {
+    return src('app/js/script.js', { sourcemaps: true })
+        .pipe(terser())
+        .pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browesersync task
+function bsInit() {
+    bs.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    watch('app/css/**/*.scss', styles);
+    watch('app/js/**/*.js').on('change', series(scripts, bs.reload));
+    watch('./*.html').on('change', bs.reload);
+}
+
+// Gulp tasks
+exports.default = series(
+    parallel(styles, scripts),
+    bsInit
+)
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.build = parallel(styles, scripts);
